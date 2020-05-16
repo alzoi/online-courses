61b1d78fd85e9b61b2c7414e0050128a3db38c3e
alzoi <alzoi@ya.ru>
2020-05-16 09:54:43 +0500

    Create find_sym.cpp

---
+++ cpp/task/find_sym.cpp
@@ -0,0 +1,61 @@
+// Поиск в последовательности суммы двух чисел, которая равна заданному числу.
+// 8
+// 1 3 4 5
+// Вывод 1.
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <set>
+
+int main() {
+
+    std::ifstream      file("input.txt");
+    std::ofstream      file_out;
+    std::string        line;
+    int                target, value, k3;
+    std::multiset<int> mst;
+
+    file_out.open("output.txt");
+
+    std::getline(file, line);
+    target = std::stoi(line);
+    
+    std::getline(file, line);
+    file.close();
+    std::stringstream lineStream(line);
+
+    while (lineStream >> value) {
+        if (value < target) {
+            mst.insert(value);
+        }        
+    }
+
+    if(!mst.size()) {
+        file_out << "0";
+        file_out.close();
+        return 0;
+    }
+
+    auto it1 = mst.begin();
+    auto it2 = mst.end();
+    it2--;
+
+    while (it1 != it2) {
+        k3 = *it1 + *it2;
+        if (k3 < target) {
+            it1++;
+        } else if (k3 > target) {
+            it2--;
+        } else {
+            file_out << "1";
+            file_out.close();
+            return 0;
+        }
+    }
+
+    file_out << "0";
+    file_out.close();
+    return 0;
+}
