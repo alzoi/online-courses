ccded9046b668aeec4933a2d14a1f890a4e504a8
alzoi <alzoi@ya.ru>
2020-05-21 16:39:53 +0500

    Update merge2.go

--- cpp/task/merge2.go
+++ cpp/task/merge2.go
@@ -13,13 +13,15 @@ import (
 	"math/rand"
 )
 
-func getInputChan(numbers []int) <-chan int {
+//func getInputChan(numbers []int) <-chan int {
+func getInputChan(numbers ...int) <-chan int {
 
 	input := make(chan int, 100)
 
 	// В отдельной горутине выполняем отправку данных массива в канал.
 	go func() {
-		for _, num := range numbers {
+		//for _, num := range numbers {
+		for num := range numbers {
 			input <- num
 			time.Sleep(time.Duration(rand.Intn(50)) * time.Millisecond)
 		}
